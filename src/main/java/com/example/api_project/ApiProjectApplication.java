3725230d651b10357f7470174a392457df3893d6
yoguoer <1269148583@qq.com>
2022-12-11 16:42:51 +0800

    初始化后端项目。

---
+++ src/main/java/com/example/api_project/ApiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/api_project/ApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
