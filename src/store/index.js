ff1c32d6f4c849933cbbf43e8b604371d188fd52
yoguoer <1269148583@qq.com>
2023-02-08 21:34:24 +0800

    FIX：user改用状态管理。

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,9 @@ import Vuex from 'vuex'
 import common from './modules/common.js'
 import menu from './modules/menu.js'
 import createPersistedState from 'vuex-persistedstate'
-import $api from '@/api/api.js'
+import user from './modules/user.js'
+
+
 Vue.use(Vuex)
 export default new Vuex.Store({
   plugins: [
@@ -19,6 +21,7 @@ export default new Vuex.Store({
   },
   modules: {
     menu,
-    common
+    common,
+    user
   }
 })
